1732a61f59322431d75b7c9580e104d89fa0e9f4
Mirjam Baltus <mirjam@fire.ly>
2020-11-17 21:44:26 +0100

    Added extra instructions and filled out example solution

--- Day1-mapping/DevDaysMapper/Mapper.cs
+++ Day1-mapping/DevDaysMapper/Mapper.cs
@@ -15,6 +15,16 @@ namespace DevDaysMapper
 
             patient.Id = record.PATIENT_ID;
 
+            // Check your data first, and change this code if you have more options or null values for gender
+            patient.Gender = (record.PATIENT_GENDER == "F") ? AdministrativeGender.Female : AdministrativeGender.Male;
+
+            patient.Name.Add(new HumanName().WithGiven(record.PATIENT_GIVENNAME).AndFamily(record.PATIENT_FAMILYNAME));
+            // This could also be accomplised without using fluent methods:
+            //var name = new HumanName();
+            //name.GivenElement.Add(new FhirString(record.PATIENT_GIVENNAME)); // given names go into a list
+            //name.Family = record.PATIENT_FAMILYNAME;
+            //patient.Name.Add(name);
+
             return patient;
         }
 
@@ -27,6 +37,22 @@ namespace DevDaysMapper
             // Unit Code:   10*3/uL
 
             Observation wbc = new Observation();
+
+            wbc.Code = new CodeableConcept("http://loinc.org", "6690-2", "Leukocytes [#/volume] in Blood by Automated count");
+
+            wbc.Value = new Quantity(decimal.Parse(record.WBC), "10*3/uL");
+
+            wbc.Subject = new ResourceReference("Patient/" + record.PATIENT_ID);
+
+            wbc.Identifier.Add(new Identifier("http://acme.org/sequence-nos", record.SEQN));
+
+            wbc.Effective = new FhirDateTime(record.TIMESTAMP);
+
+            // Mandatory field, set to 'final' since this comes from a completed dataset
+            wbc.Status = ObservationStatus.Final;
+
+            // Optionally add the category
+            wbc.Category.Add(new CodeableConcept("http://terminology.hl7.org/CodeSystem/observation-category", "laboratory", "Laboratory"));
 
             return wbc;
         }
@@ -41,6 +67,22 @@ namespace DevDaysMapper
 
             Observation rbc = new Observation();
 
+            rbc.Code = new CodeableConcept("http://loinc.org", "789-8", "Erythrocytes [#/volume] in Blood by Automated count");
+
+            rbc.Value = new Quantity(decimal.Parse(record.RBC), "10*6/uL");
+
+            rbc.Subject = new ResourceReference("Patient/" + record.PATIENT_ID);
+
+            rbc.Identifier.Add(new Identifier("http://acme.org/sequence-nos", record.SEQN));
+
+            rbc.Effective = new FhirDateTime(record.TIMESTAMP);
+
+            // Mandatory field, set to 'final' since this comes from a completed dataset
+            rbc.Status = ObservationStatus.Final;
+
+            // Optionally add the category
+            rbc.Category.Add(new CodeableConcept("http://terminology.hl7.org/CodeSystem/observation-category", "laboratory", "Laboratory"));
+
             return rbc;
         }
 
@@ -53,6 +95,22 @@ namespace DevDaysMapper
             // Unit Code:   g/dL
 
             Observation hb = new Observation();
+
+            hb.Code = new CodeableConcept("http://loinc.org", "718-7", "Hemoglobin [Mass/volume] in Blood");
+
+            hb.Value = new Quantity(decimal.Parse(record.HB), "g/dL");
+
+            hb.Subject = new ResourceReference("Patient/" + record.PATIENT_ID);
+
+            hb.Identifier.Add(new Identifier("http://acme.org/sequence-nos", record.SEQN));
+
+            hb.Effective = new FhirDateTime(record.TIMESTAMP);
+
+            // Mandatory field, set to 'final' since this comes from a completed dataset
+            hb.Status = ObservationStatus.Final;
+
+            // Optionally add the category
+            hb.Category.Add(new CodeableConcept("http://terminology.hl7.org/CodeSystem/observation-category", "laboratory", "Laboratory"));
 
             return hb;
         }
--- Day1-mapping/DevDaysMapper/Program.cs
+++ Day1-mapping/DevDaysMapper/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using CsvHelper;
 using Hl7.Fhir.Model;
 
@@ -25,9 +26,61 @@ namespace DevDaysMapper
                 var pat = map.GetPatient(r);
                 if (!patientList.Exists(p => p.Id == pat.Id))
                     patientList.Add(pat);
+
+                observationList.Add(map.GetWhiteBloodCellCount(r));
+                observationList.Add(map.GetRedBloodCellCount(r));
+                observationList.Add(map.GetHemoglobin(r));
             }
 
-            Console.WriteLine($"There are {patientList.Count} patients in the file");
+            DisplayData(patientList, observationList);
+        }
+
+        static void DisplayData(List<Patient> patients, List<Observation> observations)
+        {
+            // This can be greatly improved, but just functions as an example of obtaining data from
+            // a FHIR resource and displaying that
+
+
+            Console.WriteLine($"There are {patients.Count} patients in the file. Here is their data:");
+
+            foreach (var p in patients)
+            {
+                Console.WriteLine("-----------------------------------------------------------------------------");
+
+                Console.WriteLine("Data for patient: "+ p.Name.First().Given.First() + " " + p.Name.First().Family);
+
+                var patsObservations = observations.FindAll(o=>o.Subject.Reference == "Patient/" + p.Id);
+
+                // Split the observations based on type
+                var wbcCode = new CodeableConcept("http://loinc.org", "6690-2");
+                var rbcCode = new CodeableConcept("http://loinc.org", "789-8");
+                var hbCode = new CodeableConcept("http://loinc.org", "718-7");
+
+                var wbcObservations = patsObservations.FindAll(o => o.Code.Matches(wbcCode));
+                var rbcObservations = patsObservations.FindAll(o => o.Code.Matches(rbcCode));
+                var hbObservations = patsObservations.FindAll(o => o.Code.Matches(hbCode));
+
+                Console.WriteLine("  WBC Observations:");
+                foreach (var obs in wbcObservations)
+                {
+                    var qty = (Quantity)obs.Value;
+                    Console.WriteLine($"    {obs.Effective}  {qty.Value} {qty.Code}");
+                }
+                Console.WriteLine("  RBC Observations:");
+                foreach (var obs in rbcObservations)
+                {
+                    var qty = (Quantity)obs.Value;
+                    Console.WriteLine($"    {obs.Effective}  {qty.Value} {qty.Code}");
+                }
+                Console.WriteLine("  HB Observations:");
+                foreach (var obs in hbObservations)
+                {
+                    var qty = (Quantity)obs.Value;
+                    Console.WriteLine($"    {obs.Effective}  {qty.Value} {qty.Code}");
+                }
+            }
+            Console.WriteLine("-----------------------------------------------------------------------------");
+
         }
     }
 }
